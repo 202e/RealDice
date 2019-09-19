63674e498d5ea57a8ba0e56551ac9f07e9049ea7
202e <37431702+202e@users.noreply.github.com>
2019-09-19 01:07:18 -0700

    add configuration to change radius around player that roll result will be broadcasted

--- src/io/github/_202e/realdice/RealDice.java
+++ src/io/github/_202e/realdice/RealDice.java
@@ -3,7 +3,10 @@ package io.github._202e.realdice;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class RealDice extends JavaPlugin {
+    public static int broadcastDistance;
     @Override
     public void onEnable() {
+        saveDefaultConfig();
+        RealDice.broadcastDistance = getConfig().getInt("rollbroadcastdistance");
     }
 }
